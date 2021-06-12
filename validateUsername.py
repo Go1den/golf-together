0bb1c5e4d7bf005cc7229261ac20d92a81d25f1e
Go1den <gooneden@gmail.com>
2021-06-12 01:09:34 -0500

    Usernames 3-12 characters

--- validateUsername.py
+++ validateUsername.py
@@ -7,8 +7,8 @@ def validateUsername(username: str, parent) -> bool:
     if not username.isalnum():
         messagebox.showerror("Error", "Usernames must only contain letters and numbers.", parent=parent)
         return False
-    if len(username) < 3 or len(username) > 15:
-        messagebox.showerror("Error", "Usernames must be between 3 and 15 characters.", parent=parent)
+    if len(username) < 3 or len(username) > 12:
+        messagebox.showerror("Error", "Usernames must be between 3 and 12 characters.", parent=parent)
         return False
     if username == "System":
         messagebox.showerror("Error", "System is a reserved username. Please select another name.", parent=parent)
